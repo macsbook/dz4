468fc21382573f74a4997f3edb141f5c3758bc4b
macsbook <ilgizsaparbekov@gmail.com>
2022-10-15 17:53:01 +0600

    fgf

---
+++ app.js
@@ -0,0 +1,67 @@
+var message = document.querySelector(".message");
+var score = document.querySelector(".score");
+var buttons = document.querySelectorAll("button");
+var numberUser = document.querySelector(".number-user");
+var numberComp = document.querySelector(".number-comp");
+
+
+
+var winnerScore = [0, 0];
+
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", play);
+    buttons[i].addEventListener('click', counter)
+}
+
+function play(event) {
+    var playerSelect = event.target.innerText;
+
+    var computerSelect = Math.random();
+
+    if (computerSelect < 0.34) {
+        computerSelect = "Камень";
+    } else if (computerSelect <= 0.67) {
+        computerSelect = "Бумага";
+    } else {
+        computerSelect = "Ножницы";
+    }
+
+    var result = checkWinner(playerSelect, computerSelect);
+
+    if (result === "Игрок") {
+        winnerScore[0]++;
+    }
+
+    if (result === "Компьютер") {
+        winnerScore[1]++;
+    }
+
+    console.log("Игрок:", playerSelect, "Компьютер:", computerSelect);
+    console.log(result, winnerScore);
+}
+
+function checkWinner(player, computer) {
+    if (player === "Камень" && computer === "Бумага" ||
+        player === "Ножницы" && computer === "Камень" ||
+        player === "Бумага" && computer === "Ножницы") {
+        return "Компьютер";
+    }
+
+    if (player === "Ножницы" && computer === "Бумага" ||
+        player === "Бумага" && computer === "Камень" ||
+        player === "Камень" && computer === "Ножницы") {
+        return "Игрок";
+    }
+
+    if (player === "Ножницы" && computer === "Ножницы" ||
+        player === 'Бумага' && computer === 'Бумага' ||
+        player === 'Камень' && computer === 'Камень'){
+        return "Ничья";
+    }
+}
+function counter () {
+    numberUser.innerHTML = winnerScore[0]
+    numberComp.innerHTML = winnerScore[1]
+}
+counter()
